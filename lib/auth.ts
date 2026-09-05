f637a2d2dd2e0a8a6a27c79e1346988b707e7184
andaaree <andaaree@gmail.com>
2026-09-05 22:26:59 +0700

    fix: fallback secret for NEXTAUTH_SECRET to prevent client fetch error when missing in env

--- lib/auth.ts
+++ lib/auth.ts
@@ -105,5 +105,5 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt" as const,
     maxAge: 30 * 24 * 60 * 60, // 30 days
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET || "default_toko_pos_nextauth_secret_key_32bytes",
 }
